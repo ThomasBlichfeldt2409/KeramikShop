0d240f022c706212dc32e0235233b796b5db7591
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-04-15 15:00:20 +0200

    App.razor plus more

---
+++ KeramikShop.Client/Layout/MainLayout.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace KeramikShop.Client.Layout
+{
+    public partial class MainLayout
+    {
+    }
+}
--- KeramikShop.Client/Program.cs
+++ KeramikShop.Client/Program.cs
@@ -7,5 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7017") });
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
